c59302cb3d30b9023f80691b78710fc557a68f75
Esteban Monge <estebanmonge@riseup.net>
2016-11-15 16:55:03 -0600

    Test

--- Imap/Locale/es_ES/translations.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'Help on Mailgun integration' => 'Ayuda sobre la integración con Mailgun',
-);
-
--- Imap/Locale/ru_RU/translations.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'Help on Mailgun integration' => 'Справка о Mailgun интеграции',
-);
-
