9dd99b2e39a2e2b858a0fe7586263b89b4afdaf8
Chris <daga@epicvoyage.org>
2020-06-01 15:32:52 +0100

    Added On/Off switches. Added debug switch for command line. Emails working for me.

--- Template/integration.php
+++ Template/integration.php
@@ -1,15 +1,18 @@
-<h3><img src="<?= $this->url->dir() ?>plugins/Imap/imap-icon.png"/>&nbsp;Imap</h3>
-<div class="listing">
+<h3><img src="<?= $this->url->dir() ?>plugins/Imap/imap-icon.png" alt=""/>&nbsp;Imap</h3>
+<div class="panel listing">
 
     <h3>IMAP Configuration</h3>
+	<?= $this->form->label(t('IMAP Enabled'), 'imap_enabled')  ?>
+	<?= $this->form->select('imap_enabled', array(0 => 'No', 1 => 'Yes'), $values) ?>
+
     <?= $this->form->label(t('IMAP Server'), 'imap_server') ?>
     <?= $this->form->text('imap_server', $values) ?>
 
     <?= $this->form->label(t('IMAP Server Port'), 'imap_server_port') ?>
     <?= $this->form->text('imap_server_port', $values) ?>
 
-    <?= $this->form->label(t('IMAP Server Requires SSL (0/1)'), 'imap_server_requires_ssl') ?>
-    <?= $this->form->text('imap_server_requires_ssl', $values) ?>
+	<?= $this->form->label(t('IMAP Server Requires Valid SSL'), 'imap_server_requires_ssl')  ?>
+	<?= $this->form->select('imap_server_requires_ssl', array(0 => 'No', 1 => 'Yes'), $values) ?>
 
     <?= $this->form->label(t('IMAP Username'), 'imap_username') ?>
     <?= $this->form->text('imap_username', $values) ?>
@@ -17,23 +20,29 @@
     <?= $this->form->label(t('IMAP Password'), 'imap_password') ?>
     <?= $this->form->password('imap_password', $values) ?>
 
-    <?= $this->form->label(t("Mail prefix (Default: Add a '+' to your username)"), 'imap_mail_prefix') ?>
-    <?= $this->form->text('imap_mail_prefix', $values) ?>
+    <?= $this->form->label(t("IMAP Mail Prefix (Default: Add a '+' to your username)"), 'imap_mail_prefix') ?>
+    <?= $this->form->text('imap_mail_prefix', $values, array(),
+        isset($values['imap_username']) ?
+            array('placeholder="'.htmlentities(preg_replace('/@.*$/', '', $values['imap_username'])).'+"') :
+            array()) ?>
 
     <?= $this->form->label(t("Kanboard's XML RPC URL"), 'imap_application_url') ?>
     <?= $this->form->text('imap_application_url', $values) ?>
 
-    <?= $this->form->label(t('User ID which creates tasks'), 'imap_guest_user_id') ?>
-    <?= $this->form->text('imap_guest_user_id', $values) ?>
+	<?= $this->form->label(t('User ID which creates tasks'), 'imap_guest_user_id') ?>
+	<?= $this->form->text('imap_guest_user_id', $values) ?>
 
-    <?= $this->form->label(t('Default task priority'), 'imap_default_priority') ?>
-    <?= $this->form->text('imap_default_priority', $values) ?>
+    <?= $this->form->label(t('Default Task Priority'), 'imap_default_priority') ?>
+    <?= $this->form->text('imap_default_priority', $values, array(), array('placeholder="0"')) ?>
 
-    <?= $this->form->label(t('Email automatic reply (can reference $task_id)'), 'imap_body_message') ?>
+	<?= $this->form->label(t('Generic Note Added to Task Description'), 'imap_task_description_message') ?>
+	<?= $this->form->text('imap_task_description_message', $values) ?>
+
+    <?= $this->form->label(t('Enable Automatic Email Replies'), 'imap_automatic_replies')  ?>
+	<?= $this->form->select('imap_automatic_replies', array(0 => 'No', 1 => 'Yes'), $values) ?>
+
+    <?= $this->form->label(t('Automatic Reply Body (can reference $task_id)'), 'imap_body_message') ?>
     <?= $this->form->text('imap_body_message', $values) ?>
-
-    <?= $this->form->label(t('Generic note added to task description'), 'imap_task_description_message') ?>
-    <?= $this->form->text('imap_task_description_message', $values) ?>
 
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue">
--- cron.php
+++ cron.php
@@ -13,19 +13,42 @@ $toaddress = "";
 $ccaddress = "";
 $comment['content'] = "";
 $task['description'] = "";
+$argv_db = $argv[1];
+$debug = ($argv_db == '--debug' || $argv_db == '-d');
 
-if (isset($argv[1]) && file_exists($argv[1])) {
+if ($debug) {
+    echo "Debug mode enabled.\n";
+    $argv_db = $argv[2];
+}
+
+if (isset($argv_db) && file_exists($argv_db)) {
     // Database to connect to and default settings.
-	$database = $argv[1];
+	$database = $argv_db;
 	$imap_server_port = 0;
 	$imap_server_port_default = 993;
 	$mail_prefix = null;
+	$imap_automatic_replies = 0;
+	$imap_enabled = 0;
+	$default_priority = 0;
 
 	// Load settings.
-	$db = new SQLite3($database);
-	$results = $db->query('SELECT option,value from settings where option IN ("imap_body_message","imap_default_priority","imap_server","imap_username","imap_password","imap_server_port","imap_server_requires_ssl","imap_mail_prefix","api_token","imap_application_url","imap_guest_user_id","imap_task_description_message")');
+    if ($debug) {
+        echo "Opening ".$database.".\n";
+    }
+	$db = new SQLite3($database, SQLITE3_OPEN_READONLY);
+	$results = $db->query(
+	        'SELECT option, value 
+                    FROM settings 
+                    WHERE option IN ("imap_body_message", "imap_default_priority", "imap_server", "imap_username",
+                                     "imap_password", "imap_server_port", "imap_server_requires_ssl",
+                                     "imap_mail_prefix", "api_token", "imap_application_url", "imap_guest_user_id",
+                                     "imap_task_description_message", "imap_enabled", "imap_automatic_replies")');
+
 	while ($row = $results->fetchArray()) {
 		switch ($row['option']) {
+            case "imap_enabled":
+                $imap_enabled = intval($row['value']);
+                break;
 			case "imap_server":
 				$imap_server = $row['value'];
 				break;
@@ -58,8 +81,13 @@ if (isset($argv[1]) && file_exists($argv[1])) {
 				$imap_guest_user_id = $row['value'];
 				break;
 			case "imap_default_priority":
-				$default_priority = $row['value'];
+			    if ($row['value']) {
+                    $default_priority = $row['value'];
+                }
 				break;
+            case "imap_automatic_replies":
+                $imap_automatic_replies = intval($row['value']);
+                break;
 			case "imap_body_message":
 				$body_message_configured = $row['value'];
 				break;
@@ -70,12 +98,29 @@ if (isset($argv[1]) && file_exists($argv[1])) {
 		}
 	}
 
+	// There is no reason to hold the database connection open.
+	$db->close();
+	if ($debug) {
+	    echo "Database closed.\n";
+    }
+
+	// Turn off the rest of our processing without removing the plugin.
+	if (!$imap_enabled) {
+	    if ($debug) {
+	        echo "Plugin disabled. Exiting.";
+        }
+	    exit;
+    }
+
 	// Default to the RFC 5233 Subaddress Extension format.
 	if (!$mail_prefix) {
 	    $mail_prefix = preg_replace('/@.*$/', '', $imap_username).'+';
     }
 
 	// Connect to Kanboard via XmlRPC.
+    if ($debug) {
+        echo "Using JSONRPC endpoint of: ".$jsonrpc_url."\n";
+    }
 	$client = new JsonRPC\Client($jsonrpc_url);
 	$client->authentication($jsonrpc_auth_name, $jsonrpc_auth_token);
 	$projects_tmp = $client->execute('getAllProjects');
@@ -87,6 +132,9 @@ if (isset($argv[1]) && file_exists($argv[1])) {
 	}
 
 	// Connect to the IMAP server.
+    if ($debug) {
+        echo 'Connecting to IMAP server '.$imap_server.':'.$imap_server_port.' (SSL'.($imap_server_requires_ssl ? '' : ' not ')." required).\n";
+    }
 	$imap_server_port = $imap_server_port ? $imap_server_port : $imap_server_port_default;
 	$server = new Server($imap_server, $imap_server_port);
 	$server->setAuthentication($imap_username, $imap_password);
@@ -105,6 +153,10 @@ if (isset($argv[1]) && file_exists($argv[1])) {
             " SSL as $imap_username: ". $e->getMessage());
     }
 
+	if ($debug) {
+	    echo 'Found '.count($messages)." new message(s).\n";
+    }
+
 	// Loop through the unread email messages.
     foreach ($messages as $message) {
         $body_text = $message->getMessageBody();
@@ -115,7 +167,9 @@ if (isset($argv[1]) && file_exists($argv[1])) {
         $header = $message->getHeaders();
         $task_id = null;
 
-        // throw new Exception($message->getSubject());
+        if ($debug) {
+            echo 'New Message from '.$from['address'].': '.$message->getSubject();
+        }
 
         // Parse the important email addresses.
         if (!empty($cc)) {
@@ -124,6 +178,7 @@ if (isset($argv[1]) && file_exists($argv[1])) {
                     $ccaddress .= $ccfor['address'] . ",";
                 }
             }
+            // Remove the last comma.
             $ccaddress = substr_replace($ccaddress, "", -1);
         }
         foreach ($to as $tofor) {
@@ -146,7 +201,7 @@ if (isset($argv[1]) && file_exists($argv[1])) {
         $headers .= 'From: '.$imap_username. "\r\n" .
                 'Reply-To: <'.$imap_username.'>'. "\r\n" .
                 'To: '.$from['address']."\r\n".
-                'CC: '.$toaddress.','.$ccaddress."\r\n". // TODO: Will CC'ing ourselves create a loop?
+                'CC: '.$toaddress.','.$ccaddress."\r\n".
                 'In-Reply-To: <'.$header->message_id.'>'. "\r\n" .
                 'References: <'.$header->message_id.'>'. "\r\n" .
                 'X-Mailer: PHP/' . phpversion(). "\r\n";
@@ -171,37 +226,58 @@ if (isset($argv[1]) && file_exists($argv[1])) {
 
         // If a valid Task ID was specified, add a comment to it.
         if ($task_id && $client->getTask($task_id)) {
+            if ($debug) {
+                echo 'Found Task ID: '.$task_id.". Adding comment.\n";
+            }
             $comment['task_id'] = $task_id;
             $comment['content'] .= $body_text;
             $response = $client->createComment($comment);
 
-            $subject = "Re: Added your comment on task ".$task_id;
-            $body_message = 'I added your comment on '.$task_id.'.'.$body_message;
-            $headers .= 'Subject: '.$subject;
-            if (!mail($toaddress, $subject, $body_message, $headers)) {
-                throw new Exception('Failed to send comment confirmation.');
+            if ($imap_automatic_replies) {
+                if ($debug) {
+                    echo "Replying with confirmation.\n";
+                }
+                $subject = "Re: Added your comment on task " . $task_id;
+                $body_message = 'I added your comment on ' . $task_id . '.' . $body_message;
+                $headers .= 'Subject: ' . $subject;
+                if (!mail($toaddress, $subject, $body_message, $headers)) {
+                    throw new Exception('Failed to send comment confirmation.');
+                }
             }
         } else {
             // Try to detect the project that we want.
             if (strpos($to[0]['address'], $mail_prefix) !== false) {
                 $project_identifier = str_replace($mail_prefix, '', $to[0]['address']);
                 $project_identifier = strstr($project_identifier, '@', true);
+                if ($debug) {
+                    echo 'Found project identifier in subject line: '.$project_identifier.".\n";
+                }
                 if (isset($projects[strtoupper($project_identifier)])) {
                     $task['project_id'] = $projects[strtoupper($project_identifier)]['id'];
+                } else if ($debug) {
+                    echo "Project identifier does not seem to exist in the database.\n";
                 }
             } else {
                 if (preg_match('~<(.*?)>~', $message->getSubject(), $output)) {
                     $project_identifier = $output[1];
+                    if ($debug) {
+                        echo 'Found project identifier in subject line: '.$project_identifier.".\n";
+                    }
                     if (isset($projects[strtoupper($project_identifier)])) {
                         $task['project_id'] = $projects[strtoupper($project_identifier)]['id'];
+                    } else if ($debug) {
+                        echo "Project identifier does not seem to exist in the database.\n";
                     }
-                } else {
+                } else if ($imap_automatic_replies) {
                     // We failed to detect which project the user wanted. Send a rejection notice.
+                    if ($debug) {
+                        echo "Unable to determine project identifier. Replying with failure message.\n";
+                    }
                     $subject = "Re: ERROR: ".$message->getSubject();
                     $body_message = 'Sorry, I was not able to determine which project this was for.'.$body_message;
                     $headers .= 'Subject: '.$subject;
                     if (!mail($toaddress, $subject, $body_message, $headers)) {
-                        throw new Exception('Failed to send project lookup error message.');
+                        throw new Exception('Failed to send failure message.');
                     }
                     continue;
                 }
@@ -217,15 +293,20 @@ if (isset($argv[1]) && file_exists($argv[1])) {
             $task_id = $client->createTask($task);
 
             // Confirm to the sender that we created a new task.
-            $subject = "Re: ".$message->getSubject()." TaskID#[".$task_id."]";
-            $body_message = str_replace('$task_id', $task_id, $body_message_configured).$body_message;
-            $headers .= 'Subject: '.$subject;
-            if (!mail($toaddress, $subject, $body_message, $headers)) {
-                throw new Exception('Failed to send task creation confirmation.');
+            if ($imap_automatic_replies) {
+                if ($debug) {
+                    echo "Replying with confirmation.\n";
+                }
+
+                $subject = "Re: " . $message->getSubject() . " TaskID#[" . $task_id . "]";
+                $body_message = str_replace('$task_id', $task_id, $body_message_configured) . $body_message;
+                $headers .= 'Subject: ' . $subject;
+                if (!mail($toaddress, $subject, $body_message, $headers)) {
+                    throw new Exception('Failed to send task creation confirmation.');
+                }
             }
         }
     }
 } else {
-	throw new Exception("You must specify the database path");
+	throw new Exception("You must specify the database path.");
 }
-?>
